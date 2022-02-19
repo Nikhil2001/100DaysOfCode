e0d2a862ce04ac01f613a8e803d414c47c66a31d
Nikhil2001 <nikhilreddymara@gmail.com>
2022-02-19 10:37:33 +0100

    add switch

---
+++ language/day1/switch.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := -1
+
+	if a >= 0 {
+		fmt.Println(a, "is non negative")
+	} else {
+		fmt.Println(a, "is negative")
+	}
+
+	// same with switch
+	switch {
+	case a >= 0:
+		fmt.Println(a, "is non negative")
+	default:
+		fmt.Println(a, "is negative")
+	}
+
+	rank := 11
+	switch rank {
+	case 1:
+		fmt.Println("Rank 1")
+	case 2:
+		fmt.Println("Rank 2")
+	case 3:
+		fmt.Println("Rank 3")
+	default:
+		fmt.Println("Not top 3, but ranks will not indicate overall hardwork or talent!")
+	}
+
+	rank = 2
+	switch rank {
+	case 1, 2, 3:
+		fmt.Println("Top Ranker,Secured ", rank)
+	default:
+		fmt.Println("Not top 3, but ranks will not indicate overall hardwork or talent!")
+	}
+
+	rank = 3
+	switch rank {
+	case 1:
+		fallthrough
+	case 2:
+		fallthrough
+	case 3:
+		fmt.Println("Top 3")
+	default:
+		fmt.Println("Not top 3, but ranks will not indicate overall hardwork or talent!")
+	}
+
+	switch rank := 4; rank {
+	case 1:
+		fallthrough
+	case 2:
+		fallthrough
+	case 3:
+		fallthrough
+	case 4:
+		if rank == 4 {
+			fmt.Println("just missed Top 3")
+			break
+		}
+		fmt.Println("Top 3")
+	default:
+		fmt.Println("Not top 3, but ranks will not indicate overall hardwork or talent!")
+	}
+
+}
+
+/*OUTPUT
+nikhil@nikhil:~/job/100DaysOfCode/language/day1$ go run switch.go 
+-1 is negative
+-1 is negative
+Not top 3, but ranks will not indicate overall hardwork or talent!
+Top Ranker,Secured  2
+Top 3
+just missed Top 3
+*/
