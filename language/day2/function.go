94c02bb8d0b855980c3ac312b82a686325ce7ae8
Nikhil2001 <nikhilreddymara@gmail.com>
2022-02-23 05:09:23 +0100

    add function with multiple return

--- language/day2/function.go
+++ language/day2/function.go
@@ -12,9 +12,14 @@ func addAndSub(a,b int) (add int,sub int) {
 	return 
 } 
 
+func addSubMul(a,b int) (int, int, int) {
+	return a+b,a-b,a*b
+} 
+
 
 func main(){
 	fmt.Println(sum(1,2))
 	fmt.Println(addAndSub(1,2))	
+	fmt.Println(addSubMul(2,4))
 }
 
