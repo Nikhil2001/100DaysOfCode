ae608a9b6d6860d409364c6304c18359591de09f
Nikhil2001 <nikhilreddymara@gmail.com>
2022-02-23 05:11:14 +0100

    change function return

--- language/day2/function.go
+++ language/day2/function.go
@@ -2,24 +2,22 @@ package main
 
 import "fmt"
 
-func sum(a,b int) int {
-	return a+b
-} 
-
-func addAndSub(a,b int) (add int,sub int) {
-	add = a+b
-	sub = a-b
-	return 
-} 
-
-func addSubMul(a,b int) (int, int, int) {
-	return a+b,a-b,a*b
-} 
-
-
-func main(){
-	fmt.Println(sum(1,2))
-	fmt.Println(addAndSub(1,2))	
-	fmt.Println(addSubMul(2,4))
+func sum(a, b int) int {
+	return a + b
 }
 
+func addAndSub(a, b int) (add, sub int) {
+	add = a + b
+	sub = a - b
+	return
+}
+
+func addSubMul(a, b int) (int, int, int) {
+	return a + b, a - b, a * b
+}
+
+func main() {
+	fmt.Println(sum(1, 2))
+	fmt.Println(addAndSub(1, 2))
+	fmt.Println(addSubMul(2, 4))
+}
