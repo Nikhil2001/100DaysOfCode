acce26607df15af7d4d6f6aaf3ed087ff1789183
Nikhil2001 <nikhilreddymara@gmail.com>
2022-02-23 06:21:11 +0100

    Update function.go

--- language/day2/function.go
+++ language/day2/function.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func sum(a, b int) int {
 	return a + b
@@ -16,8 +19,26 @@ func addSubMul(a, b int) (int, int, int) {
 	return a + b, a - b, a * b
 }
 
+func PrintPi() {
+	fmt.Printf("%T %v\n", math.Pi, math.Pi)
+}
+
+func fib(n int) {
+	a, b, c := 0, 1, 0
+	fmt.Print("fib:[ ", a, b, " ")
+	for i := 2; i < n; i++ {
+		c = a + b
+		fmt.Print(c, " ")
+		a, b = b, c
+
+	}
+	fmt.Println("]")
+}
+
 func main() {
 	fmt.Println(sum(1, 2))
 	fmt.Println(addAndSub(1, 2))
 	fmt.Println(addSubMul(2, 4))
+	PrintPi()
+	fib(10)
 }
