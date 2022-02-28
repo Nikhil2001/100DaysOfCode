89517287e183ba881c7e7051ce52875e30a3d4dc
Nikhil2001 <nikhilreddymara@gmail.com>
2022-02-28 10:22:35 +0100

    main

---
+++ language/day3/main/go.mod
@@ -0,0 +1,3 @@
+module github.com/Nikhil2001/100DaysOfCode/language/day3/main
+
+go 1.17
---
+++ language/day3/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/Nikhil2001/100DaysOfCode/language/day3/calculator"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(calculator.Add(2,4))
+
+}
