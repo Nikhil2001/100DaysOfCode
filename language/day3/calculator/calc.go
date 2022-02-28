cd4f9c119e84876f50caee57ff98fd2e05861400
Nikhil2001 <nikhilreddymara@gmail.com>
2022-02-28 10:44:42 +0100

    update calc with mod

--- language/day3/calculator/calc.go
+++ language/day3/calculator/calc.go
@@ -1,5 +1,7 @@
 package calculator
 
+import "errors"
+
 func Add(a,b int) int {
 	return a+b
 }
@@ -10,4 +12,11 @@ func Sub(a,b int) int {
 
 func Mul(a,b int) int {
 	return a*b
+}
+
+func mod(a,b int) int,error {
+	if b==0{
+		return 0,errors.New("second operand cannot be zero")
+	}
+	return a%b,nil
 }
