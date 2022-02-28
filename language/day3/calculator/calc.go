4bd597cf7cbb5aa24b8dbdee7fba7a5b662c02d1
Nikhil2001 <nikhilreddymara@gmail.com>
2022-02-28 10:05:43 +0100

    add calc module

---
+++ language/day3/calculator/calc.go
@@ -0,0 +1,13 @@
+package calculator
+
+func Add(a,b int) int {
+	return a+b
+}
+
+func Sub(a,b int) int {
+	return a-b
+}
+
+func Mul(a,b int) int {
+	return a*b
+}
---
+++ language/day3/calculator/go.mod
@@ -0,0 +1,3 @@
+module github.com/100DaysOfCode/language/day3/calculator
+
+go 1.17
